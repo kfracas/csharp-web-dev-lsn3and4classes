fb1071b9e9f8f1b073552359e2d1b3148cb0e890
Kirby <kafracas@gmail.com>
2020-10-06 15:08:48 -0500

    completed exercise: classes and objects

---
+++ SchoolPractice/Course.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SchoolPractice
+{
+    class Course
+    {
+        public string Name { get; set; }
+        public int NumberOfCredits { get; set; }
+        public Dictionary<string, int> EnrolledStudents { get; set; }
+
+        public Course(string name, int numberOfCredits, Dictionary<string, int> enrolledStudents)
+        {
+            Name = name;
+            NumberOfCredits = numberOfCredits;
+            EnrolledStudents = enrolledStudents;
+        }
+    }
+}
--- SchoolPractice/Program.cs
+++ SchoolPractice/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace SchoolPractice
 {
@@ -7,6 +8,15 @@ namespace SchoolPractice
         public static void Main(string[] args)
         {
             // TODO: Instantiate your objects and test your exercise solutions with print statements here.
+            Student studentOne = new Student("Kirby", 333456, 1, 4.0);
+            Dictionary<string, int> studentIDs = new Dictionary<string, int>();
+            studentIDs.Add(studentOne.Name, studentOne.StudentId);
+            Course courseOne = new Course("Robotics", 20, studentIDs);
+            Teacher teacherOne = new Teacher("Nala", "Bear", "Animal Science", 3);
+
+            Console.WriteLine(studentOne.Name + " - ID: " + studentOne.StudentId + " Credits: " + studentOne.NumberOfCredits + " GPA: " + studentOne.Gpa);
+            Console.WriteLine(courseOne.Name + " - Credits: " + courseOne.NumberOfCredits + " Enrolled Students: " + courseOne.EnrolledStudents.Keys + courseOne.EnrolledStudents.Values);
+            Console.WriteLine(teacherOne.FirstName + teacherOne.LastName + " - Subject: " + teacherOne.Subject+ " Years Teaching: " + teacherOne.YearsTeaching);
         }
     }
 }
